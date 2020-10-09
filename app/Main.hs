805052bd5be09937f6de1fd2a14ee783b30d54a7
Dax Fohl <dax.fohl@gmail.com>
2020-10-09 11:46:06 -0700

    adjoint

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,24 @@
 module Main where
 
-import Lib
+import Data.Complex
+import Data.Complex as C
+import Data.Matrix
+import Data.Matrix as M
+
+adjoint :: Num a => Matrix (Complex a) -> Matrix (Complex a)
+adjoint = fmap conjugate . transpose
+
+helloPerson :: String -> String
+helloPerson name = "Hello" ++ " " ++ name ++ "!"
+
 
 main :: IO ()
-main = someFunc
+main = do
+  -- putStrLn "Hello! What's your name?"
+  -- name <- getLine
+  let c = 3 :+ 3
+  let c2 = 1 :+ 1
+  let m = M.fromLists [[c, c2]]
+  -- let statement = helloPerson name
+  putStrLn $ show $ m
+  putStrLn $ show $ adjoint m
